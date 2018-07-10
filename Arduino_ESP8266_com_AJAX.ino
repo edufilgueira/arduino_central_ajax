a68eb0b71b59eac052c18e5580a178d5f40811da
edufilgueira <edufilgueira@gmail.com>
2018-07-10 14:51:19 -0300

    programa esp8266

--- Arduino_ESP8266_com_AJAX.ino
+++ Arduino_ESP8266_com_AJAX.ino
@@ -83,7 +83,58 @@ void loop()
 
                 if (c == '\n' && continua )
 		{ 
-
+		  client.println("HTTP/1.1 200 OK");
+		  client.println("Content-Type: text/javascript");
+		  client.println("Access-Control-Allow-Origin: *");
+		  client.println();          
+		 
+		  int iniciofrente = linha.indexOf("?");
+		        
+		  if(iniciofrente>-1){     //verifica se o comando veio
+		    iniciofrente     = iniciofrente+6; //pega o caractere seguinte
+		    int fimfrente    = iniciofrente+4; //esse comando espero 3 caracteres
+		    String acao    = linha.substring(iniciofrente,fimfrente);//recupero o valor do comando
+		    
+		    if      ( acao == "00ON"){ digitalWrite(_pino0, LOW); } 
+		    else if ( acao == "00OF"){ digitalWrite(_pino0, HIGH);}
+		    
+		    else if ( acao == "01ON"){ digitalWrite(_pino1, LOW); } 
+		    else if ( acao == "01OF"){ digitalWrite(_pino1, HIGH);} 
+		   
+		    else if ( acao == "02ON"){ digitalWrite(_pino2, LOW); }
+		    else if ( acao == "02OF"){ digitalWrite(_pino2, HIGH);}
+		  
+		    else if ( acao == "03ON"){ digitalWrite(_pino3, LOW); }
+		    else if ( acao == "03OF"){ digitalWrite(_pino3, HIGH);}
+		  
+		    else if ( acao == "04ON"){ digitalWrite(_pino4, LOW); }
+		    else if ( acao == "04OF"){ digitalWrite(_pino4, HIGH);}
+		  
+		    else if ( acao == "05ON"){ digitalWrite(_pino5, LOW); }
+		    else if ( acao == "05OF"){ digitalWrite(_pino5, HIGH);}
+		  
+		    else if ( acao == "06ON"){ digitalWrite(_pino6, LOW); }
+		    else if ( acao == "06OF"){ digitalWrite(_pino6, HIGH);}
+		  
+		    else if ( acao == "07ON"){ digitalWrite(_pino7, LOW); }
+		    else if ( acao == "07OF"){ digitalWrite(_pino7, HIGH);}
+		  
+		    else if ( acao == "08ON"){ digitalWrite(_pino8, LOW); }
+		    else if ( acao == "08OF"){ digitalWrite(_pino8, HIGH);}
+            
+		    client.print("dados({");
+		    client.print("  _pino0 : " + digitalRead(_pino0));
+		    client.print(", _pino1 : " + digitalRead(_pino1));
+		    client.print(", _pino2 : " + digitalRead(_pino2));
+		    client.print(", _pino3 : " + digitalRead(_pino3));
+		    client.print(", _pino4 : " + digitalRead(_pino4));
+		    client.print(", _pino5 : " + digitalRead(_pino5));
+		    client.print(", _pino6 : " + digitalRead(_pino6));
+		    client.print(", _pino7 : " + digitalRead(_pino7));
+		    client.print(", _pino8 : " + digitalRead(_pino8));
+		    client.print(" })");
+		 }          
+		  break;
  		}
                 if (c == '\n') { continua = true; } 
                 else if (c != '\r') { continua = false; }
